4b17ad2a79edd04e0a8d574bb54781eb7d3f5323
DEV\NemesG1 <gergely.nemes@transuni.eu>
2023-01-28 18:39:18 +0100

    beállítások modál

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { IKendoColumn } from './oecm-table/oecm-table.component';
+import { IKendoColumn, KendoColumnType } from './oecm-table/oecm-table.component';
 import { Component } from '@angular/core';
 
 @Component({
@@ -10,7 +10,7 @@ export class AppComponent {
     public columns: IKendoColumn[] = [
         {title: 'Kód', name: 'code', hidden: false},
         {title: 'Megnevezés', name: 'name', hidden: false},
-        {title: 'Érvényesség', name: 'validity', type: 'bool', hidden: false},
+        {title: 'Érvényesség', name: 'validity', type: KendoColumnType.bool, hidden: false},
     ]
     public data = new Array(20).fill(0).map((_, i) => {
         return {
--- src/app/oecm-table/oecm-table.component.ts
+++ src/app/oecm-table/oecm-table.component.ts
@@ -1,14 +1,37 @@
-import { CompositeFilterDescriptor, filterBy, process } from "@progress/kendo-data-query"
-import { FetchDataCallback, GridComponent, GridDataResult, PageChangeEvent } from '@progress/kendo-angular-grid';
-import { Component, Input, OnInit, ViewChild } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
-import { ExcelExportData } from "@progress/kendo-angular-excel-export";
+import { Component, HostListener, Input, ViewChild } from '@angular/core';
+import { GridComponent } from '@progress/kendo-angular-grid';
+import { process } from "@progress/kendo-data-query";
+
+export enum KendoColumnType {
+    string = 'string',
+    number = 'number',
+    bool = 'bool'
+}
 
 export interface IKendoColumn {
     name: string
     title: string
-    type?: 'string' | 'number' | 'bool'
+    type?: KendoColumnType
     hidden: boolean
+}
+
+interface IKendoTableLocalConfig {
+    [columnName: string]: boolean | any
+    PAGESIZE?: number
+}
+
+class KendoColumn {
+    public type: KendoColumnType
+    public hidden: boolean
+    public title: string
+    public name: string
+
+    constructor(config: IKendoColumn, localConfig: IKendoTableLocalConfig) {
+        this.title = config.title
+        this.name = config.name
+        this.hidden = localConfig[config.name] ?? config.hidden ?? false
+        this.type = config.type ?? KendoColumnType.string
+    }
 }
 
 @Component({
@@ -18,20 +41,39 @@ export interface IKendoColumn {
 })
 export class OecmTableComponent {
     @ViewChild(GridComponent, {static: true}) gridComponent: GridComponent
-    public grigColumns: IKendoColumn[] = []
+    public settingsModalFields: {name: string, value: boolean}[] = [ ]
+    private localConfig: IKendoTableLocalConfig = { }
+    public settingsModalIsVisible: boolean = false
+    @Input('tablename') tableName: string = ''
+    public grigColumns: KendoColumn[] = [ ]
     public kendoGridSelectBy: string = 'id'
+    public settingsPageSize: number = 8
     public filterable: boolean = false
     public originalData: any[] = [ ]
     public gridViewData: any[] = [ ]
+    private tableId: string = ''
+    public pageSize: number = 8
     public skip: number = 0
-    public pageSize = 10
-    @Input('tablename') tableName: string = ''
 
     constructor() { }
 
+    ngOnDestroy(): void {
+        this.onAppUnload()
+    }
+
+    @HostListener('window:unload')
+    public onAppUnload(): void {
+        if (this.tableId) {
+            this.localConfig['PAGESIZE'] = this.pageSize
+            this.grigColumns.forEach(c => this.localConfig[c.name] = c.hidden)
+            localStorage.setItem(this.tableId, JSON.stringify(this.localConfig))
+        }
+    }
+
+
     @Input('columns')
     public set tableColumns(value: IKendoColumn[]) {
-        this.grigColumns = value
+        this.grigColumns = value.map(x => new KendoColumn(x, this.localConfig))
     }
 
     @Input('data')
@@ -43,6 +85,21 @@ export class OecmTableComponent {
     @Input('kendoGridSelectBy')
     public set setKendoGridSelectBy(value: string) {
         this.kendoGridSelectBy = value
+    }
+
+    @Input('tableid')
+    public set setTableId(value: string) {
+        this.tableId = value
+        this.localConfig = JSON.parse(localStorage.getItem(this.tableId) || '{}')
+        if (new Object(this.localConfig).hasOwnProperty('PAGESIZE')) {
+            this.pageSize = this.localConfig.PAGESIZE ?? 8
+        }
+        else {
+            this.pageSize = 8
+        }
+
+        this.localConfig.PAGESIZE = this.pageSize
+
     }
 
     public toExcel(): void {
@@ -57,8 +114,26 @@ export class OecmTableComponent {
         this.filterable = !this.filterable
     }
 
-    public toggleColumn(): void {
-        this.grigColumns[2].hidden = !this.grigColumns[2].hidden
+    public showSettingsModal(): void {
+        this.settingsModalFields = this.grigColumns.map(x => {
+            return {name: x.name, value: !x.hidden}
+        })
+        this.settingsPageSize = this.pageSize
+
+        this.settingsModalIsVisible = true
+    }
+
+    public hideSettingsModal(withSave: boolean): void {
+        this.settingsModalIsVisible = false
+
+        if (withSave) {
+            this.pageSize = this.settingsPageSize
+
+            let settingsMap: Map<string, boolean> = new Map(this.settingsModalFields.map(s => [s.name, s.value]))
+            this.grigColumns.forEach(c => {
+                c.hidden = !settingsMap.get(c.name) ?? false
+            })
+        }
     }
 
     public onFilter(event: Event): void {
@@ -77,10 +152,10 @@ export class OecmTableComponent {
         }).data
     }
 
-    private getFilterOperator(type?: string): string {
+    private getFilterOperator(type?: KendoColumnType): string {
         switch (type) {
-            case 'number': return 'eq'
-            case 'bool': return 'eq'
+            case KendoColumnType.number: return 'eq'
+            case KendoColumnType.bool: return 'eq'
         
             default: return 'contains'
         }
